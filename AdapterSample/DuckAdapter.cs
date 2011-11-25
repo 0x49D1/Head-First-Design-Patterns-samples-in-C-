51bbde0da3ca744a88cedd881b4a4915a8fc6d75
0x49D1 <0x49D1@localhost>
2011-11-25 13:23:08 +0400

    Added first pattern. Page 235. Adapter

---
+++ AdapterSample/DuckAdapter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdapterSample
+{
+    class DuckAdapter:ITurkey
+    {
+        public IDuck duck;
+
+        public DuckAdapter(IDuck d)
+        {
+            duck = d;
+        }
+
+        public void Gobble()
+        {
+            duck.Quarck();
+        }
+
+        public void Fly()
+        {
+            duck.Fly();
+        }
+    }
+}
---
+++ AdapterSample/MallardDuck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdapterSample
+{
+    class MallardDuck : IDuck
+    {
+        public void Quarck()
+        {
+            Console.WriteLine("Quack");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("I'm flying");
+        }
+    }
+}
