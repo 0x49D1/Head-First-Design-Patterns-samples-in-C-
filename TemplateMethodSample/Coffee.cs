3f77765bd099601003860d4bc57d5c7dc9879417
Dmitry Pursanov <dpursanov@live.com>
2011-11-26 14:40:33 +0400

    Implementing template method pattern

---
+++ TemplateMethodSample/Coffee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TemplateMethodSample
+{
+    class Coffee
+    {
+        public void PrepareReceipe()
+        {
+            BoilWater();
+            BrewCoffeeGrinds();
+            PourInCup();
+            AddSugarAndMilk();
+        }
+
+        private void AddSugarAndMilk()
+        {
+            Console.WriteLine("Adding Sugar and Milk");
+        }
+
+        private void PourInCup()
+        {
+            Console.WriteLine("Puring into cup");
+        }
+
+        private void BrewCoffeeGrinds()
+        {
+            Console.WriteLine("Drippin Coffee through filter");
+        }
+
+        private void BoilWater()
+        {
+            Console.WriteLine("Boiling Water");
+        }
+    }
+}
