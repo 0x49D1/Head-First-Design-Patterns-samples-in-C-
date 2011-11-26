475d28ce7cda3ba6d83321f6b65422db29a04eb3
Dmitry Pursanov <dpursanov@live.com>
2011-11-26 14:41:55 +0400

    And a tea class

---
+++ TemplateMethodSample/Tea.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TemplateMethodSample
+{
+    class Tea
+    {
+        public void PrepareReceipe()
+        {
+            BoilWater();
+            SteepTeaBag();
+            PourInCup();
+            AddLemon();
+        }
+
+        private void AddLemon()
+        {
+            Console.WriteLine("Adding lemon");
+        }
+
+        private void PourInCup()
+        {
+            Console.WriteLine("Puring into cup");
+        }
+
+        private void SteepTeaBag()
+        {
+            Console.WriteLine("Steeping the tea");
+        }
+
+        private void BoilWater()
+        {
+            Console.WriteLine("Boiling Water");
+        }
+    }
+}
